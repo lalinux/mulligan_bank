a1dd6fe5a0a1a5bbaf700d2a0c7bf29f9c8bbc2f
Eduardo García <lalinux@gmail.com>
2019-03-27 10:42:42 -0400

    Account model implementation

--- bank/models.py
+++ bank/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Account(models.Model):
@@ -6,6 +7,41 @@ class Account(models.Model):
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return "[Bank Account #%d belongs to %s, is %s and has a balance of $%d" % \
+               (
+                   self.id,
+                   self.owner.get_username(),
+                   'active' if self.active else 'not active',
+                   self.balance
+               )
+
+    def get_absolute_url(self):
+        return reverse('account_detail', kwargs={'pk': self.pk})
+
+    def deposit(self, amount):
+        if not self.active:
+            raise AttributeError("The account is not active, you cannot deposit here")
+
+        if amount > 0:
+            self.movements.create(amount=amount)
+            self.balance += amount
+            self.save()
+        else:
+            raise ValueError("Deposit amount should be > 0")
+
+    def withdrawal(self, amount):
+        if amount <= 0:
+            raise ValueError("Withdrawal amount should be > 0")
+        if amount > self.balance:
+            raise ValueError("Withdrawal amount should be more than balance")
+        self.movements.create(amount=amount * -1)
+        self.balance -= amount
+        self.save()
+
+    def record_history(self):
+        return self.movements.all()
 
 
 class Movement(models.Model):
